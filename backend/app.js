bf9eb44155b75bfd0f4d7a4fef0b7d4595f36017
gabriel <paivag171@gmail.com>
2026-09-08 20:36:25 -0300

    configura Prisma, banco e ambiente de desenvolvimento

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+
+import authRoutes from "./routes/authRoutes.js";
+import provaRoutes from "./routes/provaRoutes.js";
+import turmaRoutes from "./routes/turmaRoutes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/provas", provaRoutes);
+app.use("/api/turmas", turmaRoutes);
+
+app.get("/", (req, res) => {
+    res.json({
+        mensagem: "API CorrigeAI funcionando!"
+    });
+});
+
+export default app;
---
+++ backend/routes/turmaRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+import {
+    criarTurma,
+    listarTurmas,
+    buscarTurma,
+    atualizarTurma,
+    excluirTurma
+} from "../controllers/turmaController.js";
+
+const router = express.Router();
+
+router.post("/", criarTurma);
+
+router.get("/", listarTurmas);
+
+router.get("/:id", buscarTurma);
+
+router.put("/:id", atualizarTurma);
+
+router.delete("/:id", excluirTurma);
+
+export default router;
